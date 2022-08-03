0e9e805ede7ea98f5e7e92cbe59a70851ba32e0c
Yevhenii Lymarenko <jekilllimarenko@gmail.com>
2022-08-03 13:10:22 +0300

    render profile

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from "react";
-import ReactDOM  from "react-dom";
-import {App} from "components/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
 import './index.css';
 
 
-ReactDOM.render(<App />, document.querySelector('#root'));
 
 
 
-
-
-
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
