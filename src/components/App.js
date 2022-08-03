4faff6442f6b35dd0afcd122026b0b055b1a0329
Yevhenii Lymarenko <jekilllimarenko@gmail.com>
2022-08-03 14:32:45 +0300

    add Profile.js

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import user from './user.json';
+import { Profile } from './Profile';
+
+export const App = () => {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+/>
+    </div>
+  );
+};
