2cbd33adb66843ac9e511d90b8bdd5b4134f2adb
Yevhenii Lymarenko <jekilllimarenko@gmail.com>
2022-08-06 17:04:24 +0300

    fix some render

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from '@emotion/react'
+// import { ThemeProvider } from '@emotion/react'
 import { App } from 'components/App/App';
 import './index.css';
 
